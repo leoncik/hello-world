48433debed1d2b500b5f5e56b997b9fdfb7b2ca1
Léonard Wojcik <leonardw@tutanota.com>
2021-11-27 17:19:21 +0100

    feat: fetch user data from API

---
+++ pages/test-api/test-api.js
@@ -0,0 +1,13 @@
+// Get author of latest commits from GitHub.
+
+async function myFetchFunction() {
+  let url =
+    "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits";
+  let response = await fetch(url);
+
+  let commits = await response.json(); // read response body and parse as JSON
+
+  alert(commits[0].author.login);
+}
+
+myFetchFunction();
